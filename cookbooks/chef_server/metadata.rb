545fc39dc516bae31e9a48742f18994cb74caaf4
Irving Popovetsky <irving@getchef.com>
2016-09-29 14:06:37 -0700

    Add a wait_for_server_startup resource to prevent an observed race condition where users were added before bifrost was available
    Signed-off-by: Irving Popovetsky <irving@getchef.com>

--- cookbooks/chef_server/metadata.rb
+++ cookbooks/chef_server/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures chef server'
 long_description 'Installs/Configures chef server'
-version '0.1.0'
+version '0.1.1'
 
 depends 'chef-ingredient'
 depends 'chef-server-ctl'
