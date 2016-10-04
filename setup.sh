11e4f1ae2eefebacbf9f8332b493d4c16dc33e45
Irving Popovetsky <irving@getchef.com>
2016-10-04 15:10:47 -0700

    Fix flags for OSX so that ssh-keygen runs correctly
    Signed-off-by: Irving Popovetsky <irving@getchef.com>

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir .chef
-ssh-keygen -t rsa -N "" -f .chef/delivery-validator.pem -y
+ssh-keygen -t rsa -N "" -f .chef/delivery-validator.pem
 openssl rsa -in .chef/delivery-validator.pem -pubout -out .chef/delivery-validator.pub
